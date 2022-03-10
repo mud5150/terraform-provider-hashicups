4bf293b5696bf812b7c4aae879b4421b1407d514
Michael Frank <mud5150@hotmail.com>
2022-03-10 08:22:12 -0600

    adding auth WIP

--- hashicups/provider.go
+++ hashicups/provider.go
@@ -1,15 +1,38 @@
 package hashicups
 
 import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 // Provider -
 func Provider() *schema.Provider {
 	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"username": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("HASHICUPS_USERNAME", nil),
+			},
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("HASHICUPS_PASSWORD", nil),
+			},
+		},
 		ResourcesMap: map[string]*schema.Resource{},
 		DataSourcesMap: map[string]*schema.Resource{
 			"hashicups_coffees": dataSourceCoffees(),
 		},
 	}
+}
+
+func ProviderConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	username := d.Get("username").(string)
+	password := d.Get("password").(string)
+
+	var diags diag.Diagnostics
+
 }
